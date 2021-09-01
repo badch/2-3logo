fc09c98f8edc583124690719d04adca210ef1e11
badch <50537232+badch@users.noreply.github.com>
2021-09-01 22:51:24 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onLogoEvent(TouchButtonEvent.Released, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(500)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(500)
+    }
+})
